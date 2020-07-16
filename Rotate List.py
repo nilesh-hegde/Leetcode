c6024ddc870ab54b2b962506e49851d2cc939ab4
nilesh-hegde <nilesh.hegde1999@gmail.com>
2020-07-16 20:22:49 +0530

    Rotate the linked list K time to right

---
+++ Rotate List.py
@@ -0,0 +1,101 @@
+'''
+Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+Example 2:
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head or not head.next:
+            return head
+        def rotate(head):
+            temp=head
+            prev=None
+            while temp.next:
+                prev=temp
+                temp=temp.next
+            prev.next=None
+            temp.next=head
+            head=temp
+            return head
+        temp=head
+        count=0
+        while temp:
+            count=count+1
+            temp=temp.next
+        k=k%count
+        while k>0:
+            head=rotate(head)
+            k=k-1
+        return head
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def stringToListNode(input):
+    # Generate list from the input
+    numbers = stringToIntegerList(input)
+
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in numbers:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]"
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            head = stringToListNode(line);
+            line = next(lines)
+            k = int(line);
+            
+            ret = Solution().rotateRight(head, k)
+
+            out = listNodeToString(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
