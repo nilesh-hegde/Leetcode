518a78f77ccba1617db09b8615143fd9d3da8843
nilesh-hegde <nilesh.hegde1999@gmail.com>
2020-07-17 14:16:19 +0530

    Convert Inorder Linked List to Balanced Binary Search Tree

---
+++ Inorder to BST.py
@@ -0,0 +1,107 @@
+'''
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted linked list: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        temp=head
+        list1=[]
+        while temp:
+            list1.append(temp.val)
+            temp=temp.next
+        def convert(i,j):
+            nonlocal list1
+            if i>j:
+                return None
+            mid=int((i+j)/2)
+            node=TreeNode(list1[mid])
+            if i==j:
+                return node
+            node.left=convert(i,mid-1)
+            node.right=convert(mid+1,j)
+            return node
+        root=convert(0,len(list1)-1)
+        return root
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def stringToListNode(input):
+    # Generate list from the input
+    numbers = stringToIntegerList(input)
+
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in numbers:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+
+    ptr = dummyRoot.next
+    return ptr
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            head = stringToListNode(line);
+            
+            ret = Solution().sortedListToBST(head)
+
+            out = treeNodeToString(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
