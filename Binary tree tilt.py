8f82af457e9ad3434ce232c9eac3338fea1c2499
Nilesh Hegde <55364143+nilesh-hegde@users.noreply.github.com>
2020-09-16 22:24:06 +0530

    Create Binary tree tilt.py
    Tilt of a tree is the absolute difference between sum of node values of left subtree and right subtree

---
+++ Binary tree tilt.py
@@ -0,0 +1,42 @@
+'''
+Given a binary tree, return the tilt of the whole tree.
+
+The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values and the sum of all right subtree node values. Null node has tilt 0.
+
+The tilt of the whole tree is defined as the sum of all nodes' tilt.
+
+Example:
+Input: 
+         1
+       /   \
+      2     3
+Output: 1
+Explanation: 
+Tilt of node 2 : 0
+Tilt of node 3 : 0
+Tilt of node 1 : |2-3| = 1
+Tilt of binary tree : 0 + 0 + 1 = 1
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: TreeNode) -> int:
+        s=0
+        def traverse(node):
+            nonlocal s
+            if not node:
+                return 0
+            else:
+                l=traverse(node.left)
+                r=traverse(node.right)
+                s+=abs(l-r)
+                return node.val+l+r
+        traverse(root)
+        return s
+            
+        
+        
